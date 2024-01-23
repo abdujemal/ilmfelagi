bff7a09ae0ecd51d0b7ddfe06fe56b093514b480
Abdu Jemal <abdu1994jemal@gmail.com>
2024-01-23 18:20:57 +0300

    done

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,6 +1,4 @@
 // pages/api/preview.ts
-
-import { NextApiRequest, NextApiResponse } from 'next';
 import { getFirestore, getDoc, doc} from 'firebase/firestore';
 import app from '../../utils/firebase';
 
@@ -13,6 +11,8 @@ export default async function handler(req, res) {
   try {
     const firestore = getFirestore(app);
     const courseDoc = await getDoc(doc(firestore, "Courses", courseId));
+
+    console.log(courseId);
 
     if (!courseDoc.exists()) {
       res.status(404).json({ message: 'Course not found' });
