5321beae93f413039c3d93828a450e1d4732e3d1
Abdu Jemal <abdu1994jemal@gmail.com>
2024-01-23 17:45:30 +0300

    done

--- pages/courses/[courseId].jsx
+++ pages/courses/[courseId].jsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from 'react';
 import { useMemo } from 'react';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 
 function CoursePage({ previewData }) {
