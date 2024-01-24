f060d141005fee8262881623a5e4e309bf02f48e
Abdu Jemal <abdu1994jemal@gmail.com>
2024-01-24 15:20:09 +0300

    done

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -1,5 +1,5 @@
 // pages/api/preview.ts
-import { getFirestore, getDoc, doc, collection, getDocs} from 'firebase/firestore';
+import { getFirestore, getDoc, doc} from 'firebase/firestore';
 import app from '../../utils/firebase';
 
 // Type for preview data
@@ -10,15 +10,14 @@ export default async function handler(req, res) {
 
   try {
     const firestore = getFirestore(app);
-    const docRef = doc(firestore, "Courses", courseId);
-    const courseDoc = await getDocs(collection(firestore, "Courses"));
+    const courseDoc = await getDoc(doc(firestore, courseId, "Courses"));
 
     console.log(courseId);
 
     
 
-    if (courseDoc.length > 0) {
-      res.status(404).json({ message: `Course not found on id ${courseDoc}` });
+    if (!courseDoc.exists()) {
+      res.status(404).json({ message: `Course not found on id ${courseDoc.data()}` });
       return;
     }
 
@@ -26,17 +25,9 @@ export default async function handler(req, res) {
 
     // courseDoc.data();
 
-    let chats = [];
+    const data = courseDoc.data();
 
-
-    courseDoc.forEach((doc)=>{
-      chats = chats.concat({id: doc.id, ...doc.data()});
-  
-    })
-
-    // const data = courseDoc.data();
-
-    res.json({title: chats[0].title, description: chats[0].ustaz, image: chats[0].image,});
+    res.json({title: data.title, description: data.ustaz, image: data.image,});
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error' });
