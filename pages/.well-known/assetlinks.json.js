636124d25495f3a29ae12d211148e67b6a5191b5
Abdu Jemal <abdu1994jemal@gmail.com>
2024-02-23 15:44:55 +0300

    done

--- pages/.well-known/assetlinks.json.js
+++ pages/.well-known/assetlinks.json.js
@@ -1,10 +1,14 @@
-{
+var jsn = {
   "relation": ["delegate_permission/common.handle_all_urls"],
   "target": {
     "namespace": "android_app",
     "package_name": "com.aj.islamic_online_learning",
     "sha256_cert_fingerprints": [
-      "12:34:56:78:90:AB:CD:EF:12:34:56:78:90:AB:CD:EF:12:34:56:78:90:AB:CD:EF:12:34:56:78:90:AB:CD:EF"
+      "4E:25:B7:DF:FB:7E:A0:94:9F:31:AB:94:DD:17:E8:1A:D3:4F:F8:C5:84:1A:FD:EC:02:2E:E2:97:D6:BE:E7:A3"
     ]
   }
 }
+const AL = ()=>
+   <>
+   {jsn}
+  </>
